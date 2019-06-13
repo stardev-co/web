30598d787843739430ccf9ec89a3d9d49dc9dbfb
Chance Hudson <jchancehud@gmail.com>
2019-06-13 07:54:12 -0500

    Add postinstall script to download pdfkit

---
+++ scripts/postinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Downloading prebuilt pdfkit"
+
+wget --no-verbose -O ./static/pdfkit.standalone.js https://github.com/foliojs/pdfkit/releases/download/v0.10.0/pdfkit.standalone.js
