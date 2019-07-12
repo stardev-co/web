0313fe70a86045a9900daccc533debd563283545
Chance Hudson <jchancehud@gmail.com>
2019-07-12 12:29:23 +0200

    add github logo footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Colors from '../Colors'
+import { GithubLogo } from './Icons'
 
 export default class Footer extends React.Component<{}> {
   render() {
@@ -9,15 +10,18 @@ export default class Footer extends React.Component<{}> {
         <div
           style={{
             display: 'flex',
-            justifyContent: 'space-between',
+            justifyContent: 'center',
             alignItems: 'center',
-            height: 30,
+            minHeight: 30,
             borderTop: `1px solid ${Colors.black}`,
             backgroundColor: Colors.whiteLight,
             marginTop: 8,
+            padding: 8,
           }}
         >
-          Footer
+          <a href="https://github.com/stardev-co" target="_blank">
+            <GithubLogo style={{ fill: Colors.black }} />
+          </a>
         </div>
       </>
     )
